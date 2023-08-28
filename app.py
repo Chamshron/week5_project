38987e6c9156ff7f4366fe509e04ed771f579c52
Chamsron <emmacameron219@gmail.com>
2023-08-28 15:19:23 +0100

    players, matches and team controllers begun w link

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 from controllers.team_controller import teams_blueprint
 from controllers.match_controller import matches_blueprint
+from controllers.player_controller import players_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(teams_blueprint)
 app.register_blueprint(matches_blueprint)
+app.register_blueprint(players_blueprint)
 
 @app.route('/')
 def home():
